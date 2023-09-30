65a82d27375d0fe366ba837aed69d2135cc16519
Joshua Chang <joshchang9009@gmail.com>
2023-09-30 14:52:06 -0700

    api testing

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,26 +1,26 @@
-const { defineConfig } = require('cypress');
-const cucumber = require('cypress-cucumber-preprocessor').default;
+const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
 const createEsbuildPlugin =
-  require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
-const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const nodePolyfills =
-  require('@esbuild-plugins/node-modules-polyfill').NodeModulesPolyfillPlugin;
+  require("@esbuild-plugins/node-modules-polyfill").NodeModulesPolyfillPlugin;
 const addCucumberPreprocessorPlugin =
-  require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: "http://localhost:3000/",
     chromeWebSecurity: false,
-    specPattern: '**/*.feature',
+    specPattern: "**/*.{feature,cy.js}",
 
     async setupNodeEvents(on, config) {
       // implement node event listeners here
-      on('file:preprocessor', cucumber()); //For cypress cucumber preprocessor
+      on("file:preprocessor", cucumber()); //For cypress cucumber preprocessor
       await addCucumberPreprocessorPlugin(on, config); // to allow json to be produced
       // To use esBuild for the bundler when preprocessing
       on(
-        'file:preprocessor',
+        "file:preprocessor",
         createBundler({
           plugins: [nodePolyfills(), createEsbuildPlugin(config)],
         })
