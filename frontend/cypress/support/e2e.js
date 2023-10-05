189b5cf5e6f06d68f137b526b41a7fc62329943a
mrjoshchang <joshchang9009@gmail.com>
2023-10-04 21:43:41 -0700

    bonus lighthouse

--- frontend/cypress/support/e2e.js
+++ frontend/cypress/support/e2e.js
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import "./commands";
+import "cypress-axe";
+import "@cypress-audit/lighthouse/commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
